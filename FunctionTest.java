214a5747cfa424200119305d9e1ec21839e1ba7e
aihummel <lexa@lexahummel.net>
2017-12-30 20:31:28 -0500

    ?

--- FunctionTest.java
+++ FunctionTest.java
@@ -3,7 +3,7 @@ import java.text.DecimalFormat;
 public class FunctionTest {
 	public static void main(String[] args) throws Exception {
 		DecimalFormat df = new DecimalFormat("0.0000");
-		Function f = new Function("x^(x+1)");
+		Function f = new Function("1+");
 		for (double n = 0; n < 5; n+=1)
 			System.out.println(df.format(n)+"  "+df.format(f.get(n)));
 		/*
